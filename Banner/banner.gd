68be9191e4748d5028d4c52cd33e8fef5801cfef
Francesco Bambagioni <65393920+Bambatron@users.noreply.github.com>
2025-06-03 17:16:46 +0200

    Added mine counter

--- Banner/banner.gd
+++ Banner/banner.gd
@@ -1,22 +1,32 @@
 extends Control
 
-@onready var label = $Label
+@onready var label = $Timer_Label
 @onready var timer = $Timer
 @onready var total_time_seconds : int = 0
 
+@onready var mine_count : int
+@onready var mine_label = $Mine_Counter
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	timer.start()
-	pass # Replace with function body.
+	
+	set_mine_count(99)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_timer_timeout() -> void:
 	total_time_seconds += 1
 	var m = floor(total_time_seconds / 60) as int
 	var s = int(total_time_seconds % 60)
 	label.text = "%02d:%02d" % [m,s]
+
+func set_mine_count(new_mine_count):
+	mine_count = new_mine_count
+	mine_label.text = str(mine_count)
+
+func _on_game_setup() -> void:
+	set_mine_count(Difficulty.get_mines_per_difficulty())
+	
